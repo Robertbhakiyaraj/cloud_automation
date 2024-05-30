5154011d21adce83e353258f3f756cc7b840f87b
Bhakiyaraj <robert.bhakiyaraj@fiserv.com>
2024-05-30 10:35:50 +0530

    Added more azure code for resources with modules

--- azure/modules/network/vnet/variables.tf
+++ azure/modules/network/vnet/variables.tf
@@ -1,11 +1,23 @@
-variable "location" {
+variable "vnet_name" {
+     type = string
+     default = "example"
+     description = "Name of the Virtual Network "
+}
+
+variable "resource_group_name" {
+     type = string
+     default = "example"
+     description = "Name of the Resource Group Name "
+}
+
+variable "resource_group_location" {
      type = string
      default = "eastus2"
      description = "Location for resource group in which the resources will be created."
 }
 
-variable "vnet_name" {
-     type = string
-     default = "example"
-     description = "Name of the Virtual Network "
+variable "address_space" {
+     type = list()
+     default = ["10.0.0.0/16"]
+     description = "Location for resource group in which the resources will be created."
 }
